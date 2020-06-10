1c3e0b6933a6aa883290b9f02fcfb5c58af2fc8a
QUANG HAI <lequanghai540@gmail.com>
2020-06-10 19:33:48 +0700

    initial commit

---
+++ src/dao/SinhVienDAO.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import pojos.*;
+import util.*;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author DELL
+ */
+public class SinhVienDAO {
+
+    public static List<Sinhvien> layDanhSachSinhVien() {
+        List<Sinhvien> ds = null;
+        Session session = NewHibernateUtil.getSessionFactory()
+                .openSession();
+        try {
+            String hql = "select sv from Sinhvien sv";
+            Query query = session.createQuery(hql);
+            ds = query.list();
+        } catch (HibernateException ex) {
+            System.out.println(ex);
+        } finally {
+            session.close();
+        }
+        return ds;
+    }
+
+    public static Sinhvien laythongtinsinhvien(String maSinhVien) {
+        Sinhvien sv = null;
+        Session session = NewHibernateUtil.getSessionFactory().openSession();
+        try {
+            sv = (Sinhvien) session.get(Sinhvien.class, maSinhVien);
+        } catch (Exception ex) {
+            //Log the exception
+            System.out.println(ex);
+        } finally {
+            session.close();
+        }
+        return sv;
+    }
+
+    public static boolean themSinhVien(Sinhvien sv) {
+        Session session = NewHibernateUtil.getSessionFactory().openSession();
+        if (SinhVienDAO.laythongtinsinhvien(sv.getMssv()) != null) {
+            return false;
+        }
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(sv);
+            transaction.commit();
+        } catch (HibernateException ex) {
+            transaction.rollback();
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return true;
+    }
+
+    public static void themNhieuSinhVien(List<Sinhvien> ds) {
+        for (int i = 0; i < ds.size(); i++) {
+            System.out.println(ds.get(i).getHoten());
+            themSinhVien(ds.get(i));
+        }
+    }
+}
---
+++ src/util/myCSVFile.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import pojos.Sinhvien;
+import quanlydiemsinhviet.JInternalFrameImportdsLop;
+
+/**
+ *
+ * @author DELL
+ */
+public class myCSVFile {
+
+    public static List<Sinhvien> importDSLop(String file) {
+        List<Sinhvien> dsSV = null;
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+            dsSV = new ArrayList<Sinhvien>();
+            String str;
+            String[] s;
+            int counter = 0;
+            String lop = "";
+            while (true) {
+                str = br.readLine();
+                if (str == null) {
+                    break;
+                }
+                s = str.split(",");
+
+                //first line
+                if (counter == 0) {
+                    lop = s[0];
+                } else if (counter >= 2) {
+                    Sinhvien sv = new Sinhvien(s[1], s[2], s[3], s[4], lop);
+                    dsSV.add(sv);
+                }
+
+                counter++;
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(JInternalFrameImportdsLop.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(JInternalFrameImportdsLop.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return dsSV;
+    }
+}
